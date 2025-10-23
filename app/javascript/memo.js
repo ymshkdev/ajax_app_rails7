1eb51e19a96b33e37346329083b5b6a9a2e876d9
ymshkdev <masahiko.yonemura@outlook.jp>
2025-10-23 15:40:44 +0900

    投稿できないミスの修正

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -29,7 +29,7 @@ function post (){
       };
       const list = document.getElementById("list");
       const formText = document.getElementById("content");
-      list.insertAdjacentHTML("afterend", buildHTML = (XHR));
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
       formText.value = "";
     };
   });
